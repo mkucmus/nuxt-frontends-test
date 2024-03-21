0695494aea49072715874d34a3f5e689bf83884b
mkucmus <maciejkucmus@gmail.com>
2024-03-21 09:43:33 +0100

    test: check composable

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+  const { refreshSessionContext, sessionContext } = useSessionContext();
+  refreshSessionContext();
+</script>
 <template>
   <div>
+    <pre>{{  sessionContext }}</pre>
     <NuxtWelcome />
   </div>
 </template>
